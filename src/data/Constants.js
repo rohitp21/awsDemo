993160739bb56ab4f8e83ab44fe70cba8c1483d9
rohit2121 <rohit.pashte@kapstonellc.com>
2021-10-08 23:23:25 +0530

    latest

--- src/data/Constants.js
+++ src/data/Constants.js
@@ -1,12 +1,17 @@
 
+function getApiServer() {
+
+    return window.location.origin
+}
 //export const URLSERVER = "http://150.136.210.112:8080";
 //export const URLSERVER = "http://52.86.28.156:8000/";
 //export const URLSERVER = "https://cloudanalytics.kapstonellc.co/api/"
- 
+
 //after exp doman http://cloudanalytics.kapstonellc.co/ :::::::4 august 2021
 //export const URLSERVER = "https://cloudanalytics.kapstonellc.com/api/"
 //export const URLSERVER = "http://34.122.239.255:32599/"
-export const URLSERVER = "http://flask-svc:5000/"
+
+export const URLSERVER = getApiServer() + '/api/'
 
 
 //export const URLSERVER = "https://dev.kapstonellc.co/api/"
@@ -14,36 +19,36 @@ export const URLSERVER = "http://flask-svc:5000/"
 
 
 
-export const URLSERVER2 = URLSERVER+"cloudguard/";
-export const URL1 = URLSERVER+"cloudguard/providers";
-export const URL1_1 = URLSERVER+"cloudguard/get-accounts";
+export const URLSERVER2 = URLSERVER + "cloudguard/";
+export const URL1 = URLSERVER + "cloudguard/providers";
+export const URL1_1 = URLSERVER + "cloudguard/get-accounts";
 
 
-export const URL2 = URLSERVER+"cloudguard/accounts";
-export const URL3 = URLSERVER+"cloudguard/summary";
-export const URL4 = URLSERVER+"cloudguard/resource";
-export const URL5 = URLSERVER+"cloudguard/oci/report/user-policy";
-export const URL6 = URLSERVER+"cloudguard/oci/summary";
-export const URL7 = URLSERVER+"cloudguard/oci/identity";
+export const URL2 = URLSERVER + "cloudguard/accounts";
+export const URL3 = URLSERVER + "cloudguard/summary";
+export const URL4 = URLSERVER + "cloudguard/resource";
+export const URL5 = URLSERVER + "cloudguard/oci/report/user-policy";
+export const URL6 = URLSERVER + "cloudguard/oci/summary";
+export const URL7 = URLSERVER + "cloudguard/oci/identity";
 
-export const URL8 = URLSERVER+"cloudguard/oci/keys";
-export const URL9 = URLSERVER+"cloudguard/oci/regions";
-export const URL10 = URLSERVER+"token/login";
-export const URL11 = URLSERVER+"cloudguard/compliance/";
-export const URL12 = URLSERVER+"cloudguard/compliance/summary";
-export const URL_TOKEN_REF = URLSERVER+"token/refresh";
-export const CONTROLS = URLSERVER2+"controls";
-export const URL_FINDINGS = URLSERVER2+"findings";
-export const URL_COMMENT= URLSERVER2+"finding/comment";
-export const DEl_ACC= URLSERVER2+"delete/";
-export const CREATE_ACC= URLSERVER2+"add-account";
+export const URL8 = URLSERVER + "cloudguard/oci/keys";
+export const URL9 = URLSERVER + "cloudguard/oci/regions";
+export const URL10 = URLSERVER + "token/login";
+export const URL11 = URLSERVER + "cloudguard/compliance/";
+export const URL12 = URLSERVER + "cloudguard/compliance/summary";
+export const URL_TOKEN_REF = URLSERVER + "token/refresh";
+export const CONTROLS = URLSERVER2 + "controls";
+export const URL_FINDINGS = URLSERVER2 + "findings";
+export const URL_COMMENT = URLSERVER2 + "finding/comment";
+export const DEl_ACC = URLSERVER2 + "delete/";
+export const CREATE_ACC = URLSERVER2 + "add-account";
 
 
 //login email password - refresh and logout nothing -in body of post
 
 //images
 //local path
-export  const ICONS_AWS = "../assets/img/aws/";
+export const ICONS_AWS = "../assets/img/aws/";
 export const ICONS_OCI = "../assets/img/oci/";
 
 //date postfix
@@ -57,10 +62,10 @@ export const endDatePF = "T23:59:59.999999"
 //toasts messages
 export const TOAST_NET_ERR = "Network Error, Please try again later."
 export const TOAST_Data_Not_Available_Range = "Data is not available, Please select different dates."
-export const TOAST_SOMETHING_WENT_WRONG= "Something went wrong, Please try again."
-export const TOAST_DATA_SUCCESS= "Data loaded Successfuly."
-export const TOAST_DATA_COMBO_ERROR= "Either you have entered wrong credentials or there might be some network issue"
-export const TOAST_USERID_BLANK= "Please enter user id."
-export const TOAST_PASSWORD_BLANK= "Please enter password."
+export const TOAST_SOMETHING_WENT_WRONG = "Something went wrong, Please try again."
+export const TOAST_DATA_SUCCESS = "Data loaded Successfuly."
+export const TOAST_DATA_COMBO_ERROR = "Either you have entered wrong credentials or there might be some network issue"
+export const TOAST_USERID_BLANK = "Please enter user id."
+export const TOAST_PASSWORD_BLANK = "Please enter password."
 export const TOAST_Data_Not_Available = "Data is not available."
 export const TOAST_ACC_DEL_SUCCSESS = "Account deleted successfuly."
